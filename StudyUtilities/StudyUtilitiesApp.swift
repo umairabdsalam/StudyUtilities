60d16f2a273c9b85134c47d8ac310be3539346e8
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-08 17:03:32 +0100

    Initial Commit

---
+++ StudyUtilities/StudyUtilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyUtilitiesApp.swift
+//  StudyUtilities
+//
+//  Created by Umair Salam on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyUtilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
